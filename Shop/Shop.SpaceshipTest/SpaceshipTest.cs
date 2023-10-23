2584120743c8997167d49d225bbb30a276e9a867
Karina <karina.kulakova.97@gmail.com>
2023-10-23 15:09:49 +0300

    SpaceshipTest

---
+++ Shop/Shop.SpaceshipTest/SpaceshipTest.cs
@@ -0,0 +1,33 @@
+﻿using Shop.Core.Dto;
+using Shop.Core.ServiceInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.SpaceshipTest
+{
+    public class SpaceshipTest : TestBase
+    {
+        [Fact]
+        public async Task ShouldNot_AddEmptySpaceship_WhenReturnresult()
+        {
+            SpaceshipDto dto = new SpaceshipDto();
+
+            dto.Name = "Name";
+            dto.Type = "Type";
+            dto.Passengers = 123;
+            dto.EnginePower = 123;
+            dto.Crew = 123;
+            dto.Company = "asd";
+            dto.CargoWeight = 123;
+            dto.CreatedAt = DateTime.Now;
+            dto.ModifiedAt = DateTime.Now;
+
+            var result = await Svc<ISpaceshipServices>().Create(dto);
+
+            Assert.NotNull(result);
+        }
+    }
+}
